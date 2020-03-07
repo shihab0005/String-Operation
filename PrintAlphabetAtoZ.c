8a33508d9b5a26e4b3fe80515b0b62768c577e53
SHIHAB KHAN <41753677+shihab0005@users.noreply.github.com>
2020-03-07 09:22:31 -0800

    Add files via upload

---
+++ PrintAlphabetAtoZ.c
@@ -0,0 +1,12 @@
+/* C Program to Print Alphabets from a to z */
+#include<stdio.h>
+int main()
+{
+	char c;
+	
+	printf("Print Alphabet A to Z:\n");
+	for(c='A';c<='Z';c++)
+	{
+		printf("%c \t ",c);
+	}
+}
